9adbf2fdb55fe6caba8ae569683e6aa6ddb4e352
Igor Miyamoto <igor.kenzo.m@gmail.com>
2022-04-22 03:45:54 -0300

    funções colocadas

--- src/main.rs
+++ src/main.rs
@@ -31,6 +31,7 @@ pub fn parser(code: String) {
     // debug_lexer(lexer);
 }
 
+#[allow(dead_code)]
 fn debug_lexer(mut lexer: Lexer) {
     let mut tok : Token;
     loop {
@@ -63,34 +64,97 @@ pub fn program(lexer: &mut Lexer) {
 }
 
 pub fn block(lexer: &mut Lexer) {
+    //block ::= "{" statement-list "}"
     read_token_type(lexer, TokenType::LCol);
     statement_list(lexer);
     read_token_type(lexer, TokenType::RCol);
 }
 
 pub fn statement_list(lexer: &mut Lexer) {
+    //statement-list ::= statement*
     while lexer.peek(1) != '}' {
         statement(lexer);
     }
 }
 
 pub fn statement(lexer: &mut Lexer) {
-    num(lexer);
+    //statement ::= (assignment-statement ";"|structured-statement|var-declare-statement ";" | expression ";") 
+    expression(lexer);
 }
 
 pub fn assignment_statement(lexer: &mut Lexer) {
+    //assignment-statement ::= variable assignment-operator expression 
     read_token_type(lexer, TokenType::SemiCol);    
 }
 pub fn structured_statement(lexer: &mut Lexer) {
+    //structured-statement ::= loop-statement | conditional-statement
     read_token_type(lexer, TokenType::SemiCol);
 }
 pub fn var_declare_statement(lexer: &mut Lexer) {
+    //var-declare-statement ::= 'let' variable ':' type-specifier ( '=' expression )?
     read_token_type(lexer, TokenType::SemiCol);
 }
+
+pub fn loop_statement(lexer: &mut Lexer) {
+    //loop-statement ::= while-statement
+    while_statement(lexer);
+}
+
+pub fn conditional_statement(lexer: &mut Lexer) {
+    //conditional-statement ::= if-statement 
+    if_statement(lexer);
+}
+
+pub fn while_statement(lexer: &mut Lexer) {
+    //while-statement ::= "while" expression block
+    read_token_type(lexer, TokenType::ReservWhile);
+    expression(lexer);
+    block(lexer);
+}
+
+pub fn if_statement(lexer: &mut Lexer) {
+    //if-statement ::= "if" expression block ('else' block)?
+    read_token_type(lexer, TokenType::ReservIf);
+    expression(lexer);
+    block(lexer);
+    // if lexer.peek(1) == TokenType::ReservElse { //criar outro peek de token
+    //     read_token_type(lexer, TokenType::ReservElse);
+    //     block(lexer);
+    // }
+}
+
+
 pub fn expression(lexer: &mut Lexer) {
     // expression ::= simple-expression (relational-operator simple-expression)*
-    
+    simple_expression(lexer);
     read_token_type(lexer, TokenType::SemiCol);    
+}
+
+pub fn simple_expression(lexer: &mut Lexer) {
+    //(sign)? term (addition-operator term)* | '"' [a-zA-Z]* '"' | "'" [a-zA-z] "'"
+    num(lexer);
+}
+
+pub fn term(lexer: &mut Lexer) {
+    //term ::= factor (multiplication-operator factor)*
+    
+}
+pub fn addition_operator(lexer: &mut Lexer) {
+    //addition-operator ::= "+" | "-" | "||" 
+}
+
+pub fn factor(lexer: &mut Lexer) {
+    //factor ::= '!'* ( variable | number | string | '(' expression ')' )
+
+
+}
+pub fn multiplication_operator(lexer: &mut Lexer) {
+    //multiplication-operator ::= "*" | "/" | div | mod | "&&" 
+
+}
+
+pub fn identifier(lexer: &mut Lexer) {
+    //TokenType::identifier
 }
 
 pub fn num(lexer: &mut Lexer) {
